91315f095b850be4cd60fdb0c29521360b1408d6
Gorav <145363298+Gorav22@users.noreply.github.com>
2024-06-29 16:55:36 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -15,7 +15,7 @@ diabetes
 diabetes.columns
 
 y=diabetes['diabetes']
-x=diabetes[['pregnancies', 'glucose', 'diastolic', 'triceps', 'insulin', 'bmi','dpf', 'age', 'diabetes']]
+x=diabetes[['pregnancies', 'glucose', 'diastolic', 'triceps', 'insulin', 'bmi','dpf', 'age']]
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.7,random_state=1584)
